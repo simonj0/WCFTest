bf686f520f50d1566949032add0c20e01445b111
0x100 <0x100.me@gmail.com>
2018-11-30 15:20:23 +0100

    Add project files.

---
+++ WCFClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFServer;
+
+namespace WCFClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<IStringReverserService> pipeFactory = new ChannelFactory<IStringReverserService>(new NetNamedPipeBinding(), "net.pipe://localhost/WCFTest");
+
+            IStringReverserService pipeProxy = pipeFactory.CreateChannel();
+
+            while (true)
+            {
+                string str = Console.ReadLine();
+                Console.WriteLine(pipeProxy.ReverseString(str));
+            }
+        }
+    }
+}
---
+++ WCFServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(StringReverserService), new Uri("net.pipe://localhost")))
+            {
+                host.AddServiceEndpoint(typeof(IStringReverserService), new NetNamedPipeBinding(), "WCFTest");
+                host.Open();
+
+                Console.WriteLine("Service started.");
+                Console.ReadLine();
+
+                host.Close();
+            }
+        }
+    }
+}
